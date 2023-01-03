5553a57b4acd9a52e583ab032ff9ec7cb2c7ea55
geniee1220 <geniee1220@gmail.com>
2023-01-04 00:52:05 +0900

    Add : Receive data from a form

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({extended: true})) 
+
 
 app.listen(8080, function () {
   console.log('listening on 8080');
@@ -10,4 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + '/pages/write.html');
+});
+
+app.post('/add', function (req, res) {
+  res.send('전송 완료');
+  console.log(req.body.title);
+  console.log(req.body.date);
+});
+
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
+
+
