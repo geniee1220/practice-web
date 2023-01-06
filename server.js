fceb5f1d791c70b512ac454120a060d191164ac6
geniee1220 <geniee1220@gmail.com>
2023-01-06 18:54:44 +0900

    Add : DB collection

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ app.use(express.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient;
 
 
+let db;
 MongoClient.connect('mongodb+srv://admin:x1drTGsMJSuXATlE@cluster0.nmefy4t.mongodb.net/?retryWrites=true&w=majority',function(error,client){
   if(error) return console.log('error');
+ 
+  db = client.db('todoapp');
 
   app.listen(8080, function () {
     console.log('listening on 8080');
   });  
 })
+
+app.listen(8080, function () {
+  console.log('listening on 8080');
+});  
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -23,9 +30,13 @@ app.get('/write', function (req, res) {
 });
 
 app.post('/add', function (req, res) {
+ 
   res.send('전송 완료');
-  console.log(req.body.title);
-  console.log(req.body.date);
+  
+  db.collection('post').insertOne({title: req.body.title, date: req.body.date},function(error,res){
+    console.log('success')
+  });
+
 });
 
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
