dc0c4efacd19b0cd3715b7a27647a35d39d2ada0
geniee1220 <geniee1220@gmail.com>
2023-06-12 22:17:49 +0900

    Fix : Change the port number to fix the conflict

--- practice-web/clone-zoom/src/server.js
+++ practice-web/clone-zoom/src/server.js
@@ -1,18 +1,18 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 // import WebSocket from "ws";
 
 const app = express();
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => {
-  console.log("Listening on http://localhost:3000");
+  console.log('Listening on http://localhost:5050');
 };
 
 // app.listen(3000, handleListen);
@@ -20,7 +20,7 @@ const handleListen = () => {
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer, {
   cors: {
-    origin: ["https://admin.socket.io"],
+    origin: ['https://admin.socket.io'],
     credentials: true,
   },
 });
@@ -51,37 +51,37 @@ const countRoom = (roomName) => {
   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 };
 
-wsServer.on("connection", (socket) => {
+wsServer.on('connection', (socket) => {
   // wsServer.socketsJoin("announcement");
-  socket["nickname"] = "Anonymous";
+  socket['nickname'] = 'Anonymous';
 
   socket.onAny((event) => {
     console.log(`Socket Event: ${event}`);
     console.log(wsServer.sockets.adapter);
   });
 
-  socket.on("enter_room", (roomName, done) => {
+  socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit('room_change', publicRooms());
   });
 
-  socket.on("new_message", (message, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
+  socket.on('new_message', (message, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${message}`);
     done();
   });
 
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 
-  socket.on("disconnecting", () => {
+  socket.on('disconnecting', () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
     );
   });
 
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
   });
 });
 
@@ -113,4 +113,4 @@ wsServer.on("connection", (socket) => {
 //   });
 // });
 
-httpServer.listen(3000, handleListen);
+httpServer.listen(5050, handleListen);
