9b8bc4d028f3907dac752e9f78fc238f50d73413
geniee1220 <geniee1220@gmail.com>
2023-06-15 06:00:34 +0900

    Feat : 디바이스에 연결된 장치 리스트 호출 및 장치 전환 기능 추가

--- practice-web/clone-zoom/src/public/js/app.js
+++ practice-web/clone-zoom/src/public/js/app.js
@@ -3,30 +3,68 @@ const socket = io();
 const video = document.getElementById('video');
 const muteButton = document.getElementById('mute');
 const cameraButton = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
 
 // 스트림은 비디오와 오디오를 포함하는 미디어 데이터
 let mediaStream;
 let isMuted = false;
 let isCameraOff = false;
 
+// 유저의 카메라 목록 호출
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+    const currentCamera = mediaStream.getVideoTracks()[0];
+
+    cameras.forEach((camera) => {
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // 비동기로 미디어 데이터를 가져오는 함수
-const getMedia = async () => {
+const getMedia = async (deviceId) => {
+  const initialConstraints = {
+    audio: true,
+    video: { facingMode: 'user' },
+  };
+
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+
   try {
     // 미디어 데이터를 가져온다.
-    mediaStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
-
-    console.log(mediaStream);
+    mediaStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
 
     // 비디오 태그에 미디어 데이터를 넣는다.
     // srcObject란 비디오 태그에 미디어 데이터를 넣는 속성
     video.srcObject = mediaStream;
 
-    // 비디오를 재생한다.
-    video.play();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (error) {}
+};
+
+// 카메라 변경 이벤트
+const handleCameraChange = async () => {
+  await getMedia(camerasSelect.value);
 };
 
 // 음소거 버튼 클릭 이벤트
@@ -62,5 +100,6 @@ const handleCameraButton = () => {
 
 muteButton.addEventListener('click', handleMuteButton);
 cameraButton.addEventListener('click', handleCameraButton);
+camerasSelect.addEventListener('input', handleCameraChange);
 
 getMedia();
