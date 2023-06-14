01ffe380795ab8ba77b6161efb9a08e26bfad662
geniee1220 <geniee1220@gmail.com>
2023-06-15 08:56:40 +0900

    Feat : WebRTC로 양방향 통신 구현

--- practice-web/clone-zoom/src/public/js/app.js
+++ practice-web/clone-zoom/src/public/js/app.js
@@ -1,14 +1,21 @@
 const socket = io();
 
+// FUNCTION : 채팅방 내 기능 구현
+const call = document.getElementById('call');
+
 const video = document.getElementById('video');
 const muteButton = document.getElementById('mute');
 const cameraButton = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
 
+call.hidden = true;
+
 // 스트림은 비디오와 오디오를 포함하는 미디어 데이터
 let mediaStream;
 let isMuted = false;
 let isCameraOff = false;
+let roomName = null;
+let myPeerConnection;
 
 // 유저의 카메라 목록 호출
 const getCameras = async () => {
@@ -102,4 +109,97 @@ muteButton.addEventListener('click', handleMuteButton);
 cameraButton.addEventListener('click', handleCameraButton);
 camerasSelect.addEventListener('input', handleCameraChange);
 
-getMedia();
+// FUNCTION : 채팅방 입장 기능 구현
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
+
+// SOCKET Call 송신
+socket.on('welcome', async () => {
+  console.log('someone joined');
+
+  // 방을 생성한 주체가 offer를 생성
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+
+  socket.emit('offer', offer, roomName);
+  console.log('sent the offer');
+});
+
+// SOCKET Answer 수신
+socket.on('offer', async (offer) => {
+  // 방에 참여한 주체가 offer를 받아서 answer를 생성
+  console.log('received the offer');
+
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+
+  console.log('sent the answer');
+});
+
+socket.on('answer', (answer) => {
+  // 송신 측에서 answer를 받아서 setRemoteDescription
+  console.log('received the answer');
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+  // 송신 측에서 ice를 받아서 addIceCandidate
+  console.log('received candidate');
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Peer Connection
+const handleIce = (data) => {
+  console.log('sent candidate');
+  socket.emit('ice', data.candidate, roomName);
+};
+
+const handleAddStream = (data) => {
+  const peerVideo = document.getElementById('peerVideo');
+
+  console.log('got an stream from my peer');
+  peerVideo.srcObject = data.stream;
+};
+
+const makeConnection = () => {
+  myPeerConnection = new RTCPeerConnection();
+
+  // ICE Candidate (Internet Connectivity Establishment), 인터넷 연결 생성은 webRTC에 필요한 프로토콜을 의미하는데 멀리 떨어진 장치와 소통할 수 있게 하기 위함
+  myPeerConnection.addEventListener('icecandidate', handleIce);
+  myPeerConnection.addEventListener('addstream', handleAddStream);
+
+  // addTrack의 역할 : 트랙을 Peer Connection에 추가
+  // 첫번째 인자 : 추가할 트랙
+  // 두번째 인자 : 트랙이 속한 스트림
+  mediaStream.getTracks().forEach((track) => {
+    myPeerConnection.addTrack(track, mediaStream);
+  });
+};
+
+const initCall = async () => {
+  welcome.hidden = true;
+  call.hidden = false;
+
+  await getMedia();
+  makeConnection();
+};
+
+const handleWelcomeSubmit = async (event) => {
+  event.preventDefault();
+
+  const input = welcomeForm.querySelector('input');
+
+  // socket.emit의 각 인자의 의미
+  // 첫번째 인자: 이벤트 이름
+  // 두번째 인자: 서버로 보낼 데이터
+  // 세번째 인자: 서버로 보낼 콜백 함수
+  await initCall();
+  socket.emit('join_room', input.value);
+  roomName = input.value;
+  input.value = '';
+};
+
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
--- practice-web/clone-zoom/src/server.js
+++ practice-web/clone-zoom/src/server.js
@@ -29,63 +29,85 @@ instrument(wsServer, {
   auth: false,
 });
 
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-
-  return publicRooms;
-};
-
-const countRoom = (roomName) => {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-};
-
 wsServer.on('connection', (socket) => {
-  // wsServer.socketsJoin("announcement");
-  socket['nickname'] = 'Anonymous';
-
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-    console.log(wsServer.sockets.adapter);
+  // 프론트엔드에서 join_name 이벤트를 받으면 roomname 을 인자로 socket.join을 실행
+  // socket.join 은 socket.io 에서 제공하는 메소드로, roomname 이라는 이름의 방에 접속하게 해준다.
+  socket.on('join_room', (roomname) => {
+    socket.join(roomname);
+    socket.to(roomname).emit('welcome');
   });
 
-  socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit('room_change', publicRooms());
+  socket.on('offer', (offer, roomname) => {
+    socket.to(roomname).emit('offer', offer);
   });
 
-  socket.on('new_message', (message, room, done) => {
-    socket.to(room).emit('new_message', `${socket.nickname}: ${message}`);
-    done();
-  });
-
-  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
-
-  socket.on('disconnecting', () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
-    );
-  });
-
-  socket.on('disconnect', () => {
-    wsServer.sockets.emit('room_change', publicRooms());
+  socket.on('answer', (answer, roomname) => {
+    socket.to(roomname).emit('answer', answer);
   });
 });
 
-// websocket code
+// 소켓 io
+// instrument(wsServer, {
+//   auth: false,
+// });
+
+// const publicRooms = () => {
+//   const {
+//     sockets: {
+//       adapter: { sids, rooms },
+//     },
+//   } = wsServer;
+
+//   const publicRooms = [];
+
+//   rooms.forEach((_, key) => {
+//     if (sids.get(key) === undefined) {
+//       publicRooms.push(key);
+//     }
+//   });
+
+//   return publicRooms;
+// };
+
+// const countRoom = (roomName) => {
+//   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+// };
+
+// wsServer.on('connection', (socket) => {
+//   // wsServer.socketsJoin("announcement");
+//   socket['nickname'] = 'Anonymous';
+
+//   socket.onAny((event) => {
+//     console.log(`Socket Event: ${event}`);
+//     console.log(wsServer.sockets.adapter);
+//   });
+
+//   socket.on('enter_room', (roomName, done) => {
+//     socket.join(roomName);
+//     done();
+//     socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+//     wsServer.sockets.emit('room_change', publicRooms());
+//   });
+
+//   socket.on('new_message', (message, room, done) => {
+//     socket.to(room).emit('new_message', `${socket.nickname}: ${message}`);
+//     done();
+//   });
+
+//   socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
+
+//   socket.on('disconnecting', () => {
+//     socket.rooms.forEach((room) =>
+//       socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
+//     );
+//   });
+
+//   socket.on('disconnect', () => {
+//     wsServer.sockets.emit('room_change', publicRooms());
+//   });
+// });
+
+// 웹소켓
 // const wss = new WebSocket.Server({ server });
 // const sockets = [];
 
