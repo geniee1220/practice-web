22530932a5e3c7168ad52f3a7f48ec3c36e3ee52
geniee1220 <geniee1220@gmail.com>
2023-06-18 14:25:52 +0900

    Refactor : Breadcrumb에 상세 페이지 품목이 나오도록 수정

--- practice-react/react-shop-ts/src/components/BreadCrumbs/index.tsx
+++ practice-react/react-shop-ts/src/components/BreadCrumbs/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { ProductListParams } from '../../apis/products/model';
+import { productListState } from '../../recoil/atoms/productState';
+import { getProductById } from '../../apis/products';
 
 interface BreadcrumbsProps {
   paths: string[];
@@ -10,6 +14,18 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
   paths,
   categoryNameList,
 }) => {
+  const [productData, setProductData] = useState<ProductListParams | null>();
+  const productId = paths[paths.length - 1];
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const product = await getProductById(productId);
+      console.log(product);
+      setProductData(product);
+    };
+    getProduct();
+  }, []);
+
   return (
     <div className="text-sm breadcrumbs">
       <ul>
@@ -21,9 +37,7 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
           const categoryName = categoryNameList.find(
             (category) => category.value === path
           );
-          const label = categoryName
-            ? categoryName.label
-            : path.charAt(0).toUpperCase() + path.slice(1);
+          const label = categoryName ? categoryName.label : productData?.title;
           return (
             <li key={url}>
               <Link to={`${url}`}>{label}</Link>
