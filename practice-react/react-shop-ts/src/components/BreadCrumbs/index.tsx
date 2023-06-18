d1a3f9a9c04d39642c69a06ee249be44cfb4acfd
geniee1220 <geniee1220@gmail.com>
2023-06-18 11:11:11 +0900

    Refactor : BredaCrumbs에 Link 스타일 추가

--- practice-react/react-shop-ts/src/components/BreadCrumbs/index.tsx
+++ practice-react/react-shop-ts/src/components/BreadCrumbs/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface BreadcrumbsProps {
-  paths?: string[];
-  categoryNameList?: { label: string; value: string }[];
+  paths: string[];
+  categoryNameList: { label: string; value: string }[];
 }
 
 const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
@@ -16,16 +16,19 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
         <li>
           <Link to="/">홈</Link>
         </li>
-        {paths?.map((path, index) => {
-          const url = `/${paths?.slice(0, index + 1).join("/")}`;
-          const categoryName = categoryNameList?.find(
+        {paths.map((path, index) => {
+          const url = `/${paths.slice(0, index + 1).join('/')}`;
+          const categoryName = categoryNameList.find(
             (category) => category.value === path
           );
           const label = categoryName
             ? categoryName.label
             : path.charAt(0).toUpperCase() + path.slice(1);
-
-          return <li key={url}>{label}</li>;
+          return (
+            <li key={url}>
+              <Link to={`${url}`}>{label}</Link>
+            </li>
+          );
         })}
       </ul>
     </div>
