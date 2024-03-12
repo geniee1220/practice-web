7fcff6042c7535a4bfc75507af3499ff5814a6f9
geniee1220 <geniee1220@gmail.com>
2024-03-12 09:25:00 +0900

    InteractiveWeb-particleCanvas : 파티클 애니메이션

--- practice-interactiveWeb/particleCanvas/index.js
+++ practice-interactiveWeb/particleCanvas/index.js
@@ -6,8 +6,8 @@ console.log(ctx);
 // 캔버스의 크기를 지정해주지 않으면 기본값으로 300 * 150으로 설정된다.
 // 만약 css로 크기를 지정해주면, 캔버스 내부의 픽셀들은 늘어나지 않는다.
 // 따라서 캔버스의 크기도 함께 지정해주어야 한다.
-const canvasWidth = 300;
-const canvasHeight = 300;
+const canvasWidth = innerWidth;
+const canvasHeight = innerHeight;
 
 canvas.style.width = canvasWidth + 'px';
 canvas.style.height = canvasHeight + 'px';
@@ -41,31 +41,68 @@ ctx.scale(dpr, dpr);
 
 // ----- 3. 캔버스에 파티클 그리기
 class Particle {
-  constructor(x, y, radius) {
+  constructor(x, y, radius, vy) {
     this.x = x;
     this.y = y;
     this.radius = radius;
+    this.vy = vy;
   }
-
+  update() {
+    this.y += this.vy;
+  }
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, (Math.PI / 180) * 360);
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'orange';
     ctx.fill();
     ctx.closePath();
   }
 }
 
-const particle = new Particle(100, 100, 30);
+const TOTAL = 5;
+const random = (min, max) => Math.random() * (max - min + 1) + min;
+
+let particles = [];
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = random(0, canvasWidth);
+  const y = random(0, canvasHeight);
+  const radius = random(50, 100);
+  const vy = random(1, 5);
+  const particle = new Particle(x, y, radius, vy);
+
+  particles.push(particle);
+}
+
+let interval = 1000 / 60;
+let now, delta;
+let then = Date.now();
 
 // ----- 4. 캔버스에 파티클 애니메이션 그리기
 // requestAnimationFrame을 사용하여 애니메이션을 그려준다. 재귀 호출을 통해 애니메이션을 그려준다.
 function animate() {
   window.requestAnimationFrame(animate);
+  now = Date.now();
+  delta = now - then;
+
+  if (delta < interval) return;
 
   // 모든 프레임을 지우고 다시 그려준다.
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  particle.draw();
+
+  particles.forEach((particle) => {
+    particle.update();
+    particle.draw();
+
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.y = -particle.radius;
+      particle.x = random(0, canvasWidth);
+      particle.radius = random(50, 100);
+      particle.vy = random(1, 5);
+    }
+  });
+
+  then = now - (delta % interval);
 }
 
 animate();
