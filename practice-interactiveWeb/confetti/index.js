f411d93f794e51450a1148f91012a4fabd91ad54
geniee1220 <geniee1220@gmail.com>
2024-03-20 10:49:07 +0900

    InteractiveWeb-confetti : 콘페티 클래스 구현(2) 커스텀 색상

--- practice-interactiveWeb/confetti/index.js
+++ practice-interactiveWeb/confetti/index.js
@@ -29,9 +29,9 @@ function init() {
   });
 }
 
-function confetti({ x, y, count, deg }) {
+function confetti({ x, y, count, deg, colors }) {
   for (let i = 0; i < count; i++) {
-    particles.push(new Particle(x, y, deg));
+    particles.push(new Particle(x, y, deg, colors));
   }
 }
 
@@ -51,6 +51,8 @@ function render() {
     for (let i = particles.length - 1; i >= 0; i--) {
       particles[i].update();
       particles[i].draw(ctx);
+
+      if (particles[i].opacity < 0) particles.splice(i, 1);
     }
 
     then = now - (delta % interval);
@@ -61,9 +63,10 @@ function render() {
 window.addEventListener('click', () => {
   confetti({
     x: 0,
-    y: canvasHeight / 2,
+    y: 0.5,
     deg: -50,
     count: 10,
+    colors: ['#ff577f'],
   });
 });
 window.addEventListener('resize', init);
--- practice-interactiveWeb/confetti/js/Particle.js
+++ practice-interactiveWeb/confetti/js/Particle.js
@@ -1,7 +1,7 @@
-import { randomNumBetween } from './utils.js';
+import { randomNumBetween, hexToRgb } from './utils.js';
 
 export default class Particle {
-  constructor(x, y, deg = 0) {
+  constructor(x, y, deg = 0, colors) {
     this.angle = (Math.PI / 180) * randomNumBetween(deg - 30, deg + 30);
     this.radius = randomNumBetween(30, 100);
 
@@ -11,10 +11,21 @@ export default class Particle {
     this.friction = 0.89;
     this.gravity = 0.5;
 
-    this.x = x;
-    this.y = y;
-    this.width = 30;
-    this.height = 30;
+    this.x = x * innerWidth;
+    this.y = y * innerHeight;
+    this.width = 12;
+    this.height = 12;
+
+    this.widthDelta = randomNumBetween(0, 360);
+    this.heightDelta = randomNumBetween(0, 360);
+    this.rotate = randomNumBetween(0, 360);
+    this.rotateDelta = randomNumBetween(-1, 1);
+    this.opacity = 1;
+
+    this.colors = colors || ['#FF577F', '#FF884B', '#FFD384', '#FFF9B0'];
+    this.color = hexToRgb(
+      this.colors[Math.floor(randomNumBetween(0, this.colors.length - 1))]
+    );
   }
   update() {
     this.vy += this.gravity;
@@ -24,9 +35,27 @@ export default class Particle {
 
     this.x += this.vx;
     this.y += this.vy;
+
+    this.widthDelta += 2;
+    this.heightDelta += 2;
+
+    this.opacity -= 0.005;
+    this.rotate += this.rotateDelta;
   }
   draw(ctx) {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.translate(this.x + this.width * 1.2, this.y + this.height * 1.2);
+    ctx.rotate((Math.PI / 180) * this.rotate);
+    ctx.translate(-this.x - this.width * 1.2, -this.y - this.height * 1.2);
+
+    ctx.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.opacity})`;
+
+    ctx.fillRect(
+      this.x,
+      this.y,
+      this.width * Math.cos((Math.PI / 180) * this.widthDelta),
+      this.height * Math.sin((Math.PI / 180) * this.heightDelta)
+    );
+
+    ctx.resetTransform();
   }
 }
