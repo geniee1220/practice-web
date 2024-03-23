1295a6b98471b9f94c6621f8c48cf977525ec5e9
geniee1220 <geniee1220@gmail.com>
2024-03-23 12:30:22 +0900

    React-reactCanvas : 캔버스에 이미지 띄우기

--- practice-react/react-canvasMarker/src/containers/Canvas/index.jsx
+++ practice-react/react-canvasMarker/src/containers/Canvas/index.jsx
@@ -1,8 +1,62 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import * as S from './styles';
 
 function Canvas() {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const canvasParent = canvas.parentNode;
+    const ctx = canvas.getContext('2d');
+
+    const imageSrcs = ['https://source.unsplash.com/random'];
+    const loadedImages = [];
+    let currIndex = 0;
+    let prevPos = { x: 0, y: 0 };
+    let isChanging = false;
+
+    let canvasWidth, canvasHeight;
+
+    function resize() {
+      canvasWidth = canvasParent.clientWidth;
+      canvasHeight = canvasParent.clientHeight;
+      canvas.style.width = canvasWidth + 'px';
+      canvas.style.height = canvasHeight + 'px';
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+
+      preloadImages().then(() => drawImage());
+    }
+
+    function preloadImages() {
+      return new Promise((resolve, reject) => {
+        let loaded = 0;
+        imageSrcs.forEach((src) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = () => {
+            loaded += 1;
+            loadedImages.push(img);
+            if (loaded === imageSrcs.length) return resolve();
+          };
+        });
+      });
+    }
+
+    function drawImage() {
+      isChanging = true;
+      const image = loadedImages[currIndex];
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
+
+    window.addEventListener('resize', resize);
+    resize();
+
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
 
   return (
     <S.CanvasContainer>
