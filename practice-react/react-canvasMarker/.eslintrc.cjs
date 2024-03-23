f246c39805c636513fbd7b1534d27223c900d81d
geniee1220 <geniee1220@gmail.com>
2024-03-23 12:08:28 +0900

    React-reactCanvas : 기본 세팅

--- practice-react/react-canvasMarker/.eslintrc.cjs
+++ practice-react/react-canvasMarker/.eslintrc.cjs
@@ -2,14 +2,14 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
