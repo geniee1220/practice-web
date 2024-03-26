4d23a2f291080c1587135cef6cd09da00aecb7a9
geniee1220 <geniee1220@gmail.com>
2024-03-26 10:02:18 +0900

    React-reactCanvas : 에디터 회전, 반전(1)

--- practice-react/react-canvasMarker/src/containers/EditorCanvas/index.jsx
+++ practice-react/react-canvasMarker/src/containers/EditorCanvas/index.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as S from './styles';
 
 function EditorCanvas() {
   const canvasRef = useRef(null);
+  const [rotation, setRotation] = useState(0);
+  const [flipped, setFlipped] = useState(false);
 
   const divStyle = {
     display: 'flex',
@@ -24,7 +26,6 @@ function EditorCanvas() {
 
     const imageSrcs = ['https://source.unsplash.com/random'];
     const loadedImages = [];
-    let currIndex = 0;
     let canvasWidth, canvasHeight;
 
     function resize() {
@@ -34,8 +35,6 @@ function EditorCanvas() {
       canvas.style.height = canvasHeight + 'px';
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       preloadImages().then(() => drawImage());
     }
@@ -56,9 +55,20 @@ function EditorCanvas() {
     }
 
     function drawImage() {
-      const image = loadedImages[currIndex];
+      const image = loadedImages[0];
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+      ctx.rotate((rotation * Math.PI) / 180);
+      if (flipped) ctx.scale(-1, 1);
+      ctx.drawImage(
+        image,
+        -canvasWidth / 2,
+        -canvasHeight / 2,
+        canvasWidth,
+        canvasHeight
+      );
+      ctx.restore();
     }
 
     window.addEventListener('resize', resize);
@@ -67,14 +77,26 @@ function EditorCanvas() {
     return () => {
       window.removeEventListener('resize', resize);
     };
-  }, []);
+  }, [rotation, flipped]);
 
   return (
     <>
       <div style={divStyle}>
-        <button style={buttonStyle}>rotate</button>
-        <button style={buttonStyle}>revert</button>
-        <butto style={buttonStyle}>reset</butto>
+        <button style={buttonStyle} onClick={() => setRotation(rotation + 90)}>
+          Rotate
+        </button>
+        <button style={buttonStyle} onClick={() => setFlipped(!flipped)}>
+          Flip
+        </button>
+        <button
+          style={buttonStyle}
+          onClick={() => {
+            setRotation(0);
+            setFlipped(false);
+          }}
+        >
+          Reset
+        </button>
       </div>
       <S.CanvasWrapper>
         <S.CanvasContainer>
