4318b216e9f843e9b5265631795d7c11371c1917
geniee1220 <geniee1220@gmail.com>
2024-04-03 15:31:00 +0900

    NextJS-learn14: 영화 데이터 가져오기

--- practice-nextJS/nextjs-learn14/app/(home)/page.tsx
+++ practice-nextJS/nextjs-learn14/app/(home)/page.tsx
@@ -1,9 +1,23 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
+
+import { useEffect, useState } from 'react';
 
 function Page() {
-  return <div>Next14</div>;
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomardcoders.workers.dev/movies'
+    );
+    const data = await response.json();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{JSON.stringify(movies)}</div>;
 }
 
 export default Page;
