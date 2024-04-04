3bc587f78e278dc1fd476ac1ea6610879904d1a8
geniee1220 <geniee1220@gmail.com>
2024-04-04 19:04:04 +0900

    NextJS-learn14: 영화 데이터 fetch 엔드포인트 수정

--- practice-nextJS/nextjs-learn14/app/(home)/page.tsx
+++ practice-nextJS/nextjs-learn14/app/(home)/page.tsx
@@ -3,21 +3,23 @@
 import { useEffect, useState } from 'react';
 
 function Page() {
-  const [movies, setMovies] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
     const response = await fetch(
-      'https://nomad-movies.nomardcoders.workers.dev/movies'
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
     );
     const data = await response.json();
-    setMovies(data.results);
+    setMovies(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getMovies();
   }, []);
 
-  return <div>{JSON.stringify(movies)}</div>;
+  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
 }
 
 export default Page;
