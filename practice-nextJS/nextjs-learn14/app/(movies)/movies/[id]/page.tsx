d6043d0d0433d914e866b4e6f4f8481688f659c1
geniee1220 <geniee1220@gmail.com>
2024-04-05 11:32:00 +0900

    NextJS-learn14: Parallel Requests

--- practice-nextJS/nextjs-learn14/app/(movies)/movies/[id]/page.tsx
+++ practice-nextJS/nextjs-learn14/app/(movies)/movies/[id]/page.tsx
@@ -1,5 +1,21 @@
-function MoviePage({ params: { id } }: { params: { id: string } }) {
-  return <div>Movie {id}</div>;
+import { API_URL } from '../../../(home)/page';
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = await response.json();
+  return json;
+}
+
+async function getVideos(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  const json = await response.json();
+  return json;
+}
+
+async function MoviePage({ params: { id } }: { params: { id: string } }) {
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+  return <div>{movie.title}</div>;
 }
 
 export default MoviePage;
