4913e08d44e91e3fee59050680f3cc95bd5604fc
geniee1220 <geniee1220@gmail.com>
2024-04-19 08:13:03 +0900

    Typescript-blockchain : 타입스크립트로 블록체인 만들기

--- practice-typescript/typescript-blockchain/src/index.ts
+++ practice-typescript/typescript-blockchain/src/index.ts
@@ -1,7 +1,52 @@
-import { init, exit } from 'myPackage';
+import crypto from 'crypto';
 
-init({
-  url: 'http://example.com',
-});
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-exit(0);
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash = (prevHash: string, height: number, data: string) => {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash('sha256').update(toHash).digest('hex');
+  };
+}
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPreviousHash = () => {
+    if (this.blocks.length === 0) {
+      return '';
+    }
+    return this.blocks[this.blocks.length - 1].hash;
+  };
+  public addBlock = (data: string): void => {
+    const newBlock = new Block(
+      this.getPreviousHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  };
+  public getBlocks = () => [...this.blocks];
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock('First Block');
+blockchain.addBlock('Second Block');
+blockchain.addBlock('Third Block');
+
+console.log(blockchain.getBlocks());
