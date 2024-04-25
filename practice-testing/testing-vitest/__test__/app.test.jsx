31e528e6b62ef3d118dda14540f703b9b724170c
geniee1220 <geniee1220@gmail.com>
2024-04-25 19:13:14 +0900

    Testing-vitest : 테스팅 환경 구축

---
+++ practice-testing/testing-vitest/__test__/app.test.jsx
@@ -0,0 +1,30 @@
+import { describe, it, expect } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "../src/App";
+
+describe("App Component", () => {
+  it("로고 렌더링 확인", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const viteLogo = screen.getByAltText("Vite logo");
+    const reactLogo = screen.getByAltText("React logo");
+
+    // Assert
+    expect(viteLogo).toBeInTheDocument();
+    expect(reactLogo).toBeInTheDocument();
+  });
+
+  it("버튼 클릭 시 카운트 증가", () => {
+    // Arrange
+    render(<App />);
+    const countButton = screen.getByText(/count is/i);
+
+    // Act
+    fireEvent.click(countButton);
+
+    // Assert
+    expect(countButton).toHaveTextContent("count is 1");
+  });
+});
--- practice-testing/testing-vitest/vite.config.js
+++ practice-testing/testing-vitest/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    include: ["__test__/*.{test,spec}.{js,jsx,ts,tsx}"],
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["@testing-library/jest-dom"],
+  },
+});
